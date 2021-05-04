1b8e2727bc232941476413e42a241d5ed10bf75a
Bettina Carrizo <betticarrizo@gmail.com>
2021-05-04 15:26:59 -0300

    Project setup + Nutrient & User basic models

---
+++ lib/domain/model/nutrient.dart
@@ -0,0 +1,12 @@
+class Nutrient {
+  final String shortDesc;
+  final String longDesc;
+  final String foodList; //This will be changed soon
+  final String facilitators;
+
+  const Nutrient(
+      {required this.shortDesc,
+      required this.longDesc,
+      required this.foodList,
+      required this.facilitators});
+}
---
+++ lib/domain/model/user.dart
@@ -0,0 +1,12 @@
+class User {
+  final String gender;
+  final String age;
+  final String veggieType;
+  final String userType;
+
+  const User(
+      {required this.gender,
+      required this.age,
+      required this.veggieType,
+      required this.userType});
+}
